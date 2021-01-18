974a5c74c8be4988cb409254144d7131118fe593
ksiu98 <kimsiwoo98@gmail.com>
2021-01-18 23:31:03 +0900

    405 NOT ALLOWED 오류

--- ranking.php
+++ ranking.php
@@ -1,13 +1,11 @@
 <?php
-
 $conn = mysqli_connect(
     'localhost:3306',
     'root',
     'kim54951',
     'test',
         '3306');
-# title, description 이라는 사용자가 입력한 정보를 그대로 php에 입력하는 행위는 보안에 취약, 따라서 관리 필요
-
+# 사용자가 입력한 정보를 그대로 php에 입력하는 행위는 보안에 취약, 따라서 관리 필요
 $filtered = array(
     'name' => mysqli_real_escape_string($conn, $_POST['name']),
     'score' => mysqli_real_escape_string($conn, $_POST['score'])
@@ -36,3 +34,6 @@ if ($result === false) {
 // 조건에 맞게 성공적으로 서버에 입력되면 랭킹을 바로 표시해줌.
 
 // github에서 제공하는 웹호스팅은 php파일은 안된다고 한다.. 그래서 405 에러가 뜨는듯
+// php, jsp와 같은 동적웹사이트는 깃허브에서는 웹호스팅 지원안함.
+// html, css, js와 같은 정적 웹사이트만 지원
+// php 문제를 해결하려면 별도의 유료 웹 호스팅을 지원하는 곳에서 해야...
